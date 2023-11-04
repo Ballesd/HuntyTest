e458ad66f3d988f699a7194c7aa06dd3ce66cffe
Ballesd <jballesterosd@unal.edu.co>
2023-11-04 15:02:22 -0500

    Generación de las APIS de operaciones matemáticas básicas con Python.

--- README.md
+++ README.md
@@ -19,13 +19,11 @@ Levantar el contenedor y empezar a programar.
 **Todos estos comandos deben ser ejecutados en la raiz del proyecto**
 
 * Cada vez que haga cambios al contenedor, debe construirlo nuevamente:
-  
-    
+```
     docker compose build
-  
+```
 
-* Levantar el contenedor:
-  
-  
+* Levantar el contenedor: 
+```  
     docker compose up
-  
+```
--- app/main.py
+++ app/main.py
@@ -3,5 +3,23 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
+
 def index():
-    return {"title": "Hello Coder follower :)"}
+    return {"title": "Hola, ruta de inicio de la prueba de Hunty:)"}
+
+##Matematic operations with two numbers in parameters
+@app.get("/add")
+def add(a: float, b: float):
+    return a + b
+
+@app.get("/substract")
+def substract(a: float, b: float):
+    return a - b
+
+@app.get("/multiply")
+def multiply(a: float, b: float):
+    return a * b
+
+@app.get("/divide")
+def divide(a: float, b: float):
+    return a / b
