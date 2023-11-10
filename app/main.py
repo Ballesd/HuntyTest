ce897cbed5b22df22204793fa73c223df7aa4e06
Ballesd <jballesterosd@unal.edu.co>
2023-11-10 10:53:59 -0500

    Cambios en el cors

--- app/main.py
+++ app/main.py
@@ -1,5 +1,17 @@
 from fastapi import FastAPI, HTTPException, APIRouter
 from routers.operations import router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.include_router(router)
+
+@app.get("/")
+async def root():
+    return {"message": "Hola prueba de Hunty"}
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
