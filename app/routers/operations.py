0903e43681e56d2d5dd014c78f0f24998ed6bc33
Ballesd <jballesterosd@unal.edu.co>
2023-11-19 19:54:31 -0500

    Corrections to the operations.py file for validation of the input data

--- app/routers/operations.py
+++ app/routers/operations.py
@@ -6,55 +6,39 @@ router = APIRouter(
 )
 
 @router.get("/")
-
 async def index():
     return {"title": "Hola, ruta de inicio de la prueba de Hunty:)"}
 
-#Función que valida que los parámetros sean números o no sean valores nulos
-async def typeValidation(a, b):
-    if a is None or b is None:
+def validate_numbers(a, b):
+    try:
+        float(a)
+        float(b)
+    except ValueError:
         return False
-    else:
-        try:
-            float(a)
-            float(b)
-        except ValueError:
-            return False
-        else:
-            return True
+    return True
 
-# ----------------------------Funciones de operaciones matemáticas---------------------------   
-# suma
 @router.get("/add")
 async def add(a, b):
-    if typeValidation(a, b):
-        return {"resultado": float(a) + float(b)}
-    else:
+    if not validate_numbers(a, b):
         raise HTTPException(status_code=400, detail="Ingresar valores numéricos para la suma y no pueden ser nulos")
-    
-# resta
-@router.get("/substract")
-async def substract(a, b):
-    if typeValidation(a, b):
-        return {"resultado": float(a) - float(b)}
-    else:
-        raise HTTPException(status_code=400, detail="Ingresar valores numéricos para la resta y no pueden ser nulos")
+    return {"resultado": float(a) + float(b)}
 
-# multiplicación
+@router.get("/subtract")
+async def subtract(a, b):
+    if not validate_numbers(a, b):
+        raise HTTPException(status_code=400, detail="Ingresar valores numéricos para la resta y no pueden ser nulos")
+    return {"resultado": float(a) - float(b)}
+
 @router.get("/multiply")
 async def multiply(a, b):
-    if typeValidation(a, b):
-        return {"resultado": float(a) * float(b)}
-    else:
+    if not validate_numbers(a, b):
         raise HTTPException(status_code=400, detail="Ingresar valores numéricos para la multiplicación y no pueden ser nulos")
+    return {"resultado": float(a) * float(b)}
 
-# división
 @router.get("/divide")
 async def divide(a, b):
-    if typeValidation(a, b):
-        if float(b) == 0:
-            raise HTTPException(status_code=400, detail="No se puede dividir entre 0")
-        else:
-            return {"resultado": float(a) / float(b)}
-    else:
+    if not validate_numbers(a, b):
         raise HTTPException(status_code=400, detail="Ingresar valores numéricos para la división y no pueden ser nulos")
+    if float(b) == 0:
+        raise HTTPException(status_code=400, detail="No se puede dividir entre 0")
+    return {"resultado": float(a) / float(b)}
